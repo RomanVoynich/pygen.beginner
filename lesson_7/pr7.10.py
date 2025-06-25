09e8c2298907d7f1bda65a67e628cfa3b6a0a704
Roman_Voynich <w947@gmail.com>
2025-06-25 12:45:28 +0300

    i'm do it

---
+++ lesson_7/pr7.10.py
@@ -0,0 +1,6 @@
+total = 0
+
+for _ in range(10):
+    n = int(input())
+    total += n
+print('total = ', total)
---
+++ lesson_7/pr7.8.py
@@ -0,0 +1,7 @@
+count = 0
+
+for _ in range(10):
+    n = int(input())
+    if n % 2 == 0:
+        count = count + 1
+print('number count = ', count)
